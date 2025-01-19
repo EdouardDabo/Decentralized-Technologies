eb33c2cf5b9cdd96b805f9601ce7d0a45dfcb296
Skyn0xx <sacha.crochet@gmail.com>
2025-01-19 21:46:10 +0100

    setup with the right name

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
+
 setup(
-name='GameOfLife',
-version='1.0',
-author='EdouardDabo',
-license='MIT',
-long_description=open('README.md').read(),
+    name='GameOfLife-CDOF3',
+    version='1.0',
+    author='EdouardDabo',
+    license='MIT',
+    long_description=open('README.md').read(),
 )
